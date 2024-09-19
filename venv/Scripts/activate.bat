0bce48c93f47df372cae51222e8fdcfdd1194d37
Jossy14 <josiascontrerasm@gmail.com>
2024-09-18 21:52:58 -0500

    hola

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USUARIO\Desktop\J2k\J2K-IA\venv
+set VIRTUAL_ENV=C:\Users\Josia\OneDrive\Documentos\J2K-IA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 

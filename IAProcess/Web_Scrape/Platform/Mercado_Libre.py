aa38aa8a0b316fe7cc1bb1dbea2df13120b32f6d
Jostin Banquez <179901293+JostinBz16@users.noreply.github.com>
2024-10-03 20:55:57 -0500

    actualizacion de prueba

--- IAProcess/Web_Scrape/Platform/Mercado_Libre.py
+++ IAProcess/Web_Scrape/Platform/Mercado_Libre.py
@@ -8,15 +8,21 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 # Función para hacer scraping de los listados de productos en Mercado Libre
 def mercado_libre(nombre_producto):
     articulo = nombre_producto
-    r = requests.get("https://listado.mercadolibre.com.co/{}#D[A:{}]".format(articulo.replace(' ', '-'), articulo.replace(' ', '-')))
+    r = requests.get(
+        "https://listado.mercadolibre.com.co/{}#D[A:{}]".format(
+            articulo.replace(" ", "-"), articulo.replace(" ", "-")
+        )
+    )
     contenido = r.content
 
-    soup = BeautifulSoup(contenido, 'html.parser')
+    soup = BeautifulSoup(contenido, "html.parser")
 
     # Última página
     try:
-        pagination = soup.find('ul', {'class': 'andes-pagination'}).find_all('li')
-        last_page_modified = int(pagination[-2].text)  # Obtenemos la penúltima entrada que contiene el número de la última página
+        pagination = soup.find("ul", {"class": "andes-pagination"}).find_all("li")
+        last_page_modified = int(
+            pagination[-2].text
+        )  # Obtenemos la penúltima entrada que contiene el número de la última página
     except:
         last_page_modified = 1  # En caso de error, asumir que solo hay una página
 
@@ -24,79 +30,99 @@ def mercado_libre(nombre_producto):
     products_array = []
 
     for page in range(0, last_page_modified):
-        result = requests.get('https://listado.mercadolibre.com.co/{}_Desde_{}_NoIndex_True'.format(articulo.replace(' ', '-'), (page*50)+1))
+        result = requests.get(
+            "https://listado.mercadolibre.com.co/{}_Desde_{}_NoIndex_True".format(
+                articulo.replace(" ", "-"), (page * 50) + 1
+            )
+        )
         content_pagination = result.content
-        soup_pagination = BeautifulSoup(content_pagination, 'html.parser')
+        soup_pagination = BeautifulSoup(content_pagination, "html.parser")
 
         # Asegúrate de usar el selector correcto para los productos
-        alldivs = soup_pagination.find_all('div', {'class': 'ui-search-result__wrapper'})  # Cambiar selector por el actual
+        alldivs = soup_pagination.find_all(
+            "div", {"class": "ui-search-result__wrapper"}
+        )  # Cambiar selector por el actual
 
         product_urls = []  # Lista para almacenar los enlaces de productos
         for item in alldivs:
             data = {}
-            data['nombre_articulo'] = item.find('h2', {'class': 'poly-box poly-component__title'}).text
-            data['precio'] = item.find('span', {'class': 'andes-money-amount'}).text
+            data["nombre_articulo"] = item.find(
+                "h2", {"class": "poly-box poly-component__title"}
+            ).text
+            data["precio"] = item.find("span", {"class": "andes-money-amount"}).text
 
             # Extraer el precio anterior
-            precio_antes_tag = item.find('span', {'class': 'andes-money-amount__fraction'})
+            precio_antes_tag = item.find(
+                "span", {"class": "andes-money-amount__fraction"}
+            )
             if precio_antes_tag:
-                data['precio_antes'] = precio_antes_tag.text.strip()  # Extrae y limpia el texto del precio anterior
+                data["precio_antes"] = (
+                    precio_antes_tag.text.strip()
+                )  # Extrae y limpia el texto del precio anterior
             else:
-                data['precio_antes'] = None  # Si no se encuentra, asigna None
+                data["precio_antes"] = None  # Si no se encuentra, asigna None
 
-            data['link'] = item.find('a', {'class': ''})['href']
-            product_urls.append(data['link'])  # Agregar el enlace del producto a la lista
+            data["link"] = item.find("a", {"class": ""})["href"]
+            product_urls.append(
+                data["link"]
+            )  # Agregar el enlace del producto a la lista
 
             # Encuentra el span que contiene la calificación
-            rating_tag = item.find('span', {'class': 'poly-reviews__rating'})
+            rating_tag = item.find("span", {"class": "poly-reviews__rating"})
             if rating_tag:
-                data['calificacion'] = rating_tag.text.strip()
+                data["calificacion"] = rating_tag.text.strip()
             else:
-                data['calificacion'] = None  # Si no se encuentra el elemento
-                
+                data["calificacion"] = None  # Si no se encuentra el elemento
+
             # Navegar al enlace del producto para extraer la imagen
-            product_url = data['link']
+            product_url = data["link"]
             try:
                 response = requests.get(product_url)
                 if response.status_code == 200:
-                    product_soup = BeautifulSoup(response.content, 'html.parser')
+                    product_soup = BeautifulSoup(response.content, "html.parser")
 
                     # Extraer la imagen del producto
-                    imagen_tag = product_soup.find('img', {'class': 'ui-pdp-image ui-pdp-gallery__figure__image'})
-                    if imagen_tag and 'src' in imagen_tag.attrs:
-                        data['imagen'] = imagen_tag['src']  # Extraer la URL de la imagen
+                    imagen_tag = product_soup.find(
+                        "img", {"class": "ui-pdp-image ui-pdp-gallery__figure__image"}
+                    )
+                    if imagen_tag and "src" in imagen_tag.attrs:
+                        data["imagen"] = imagen_tag[
+                            "src"
+                        ]  # Extraer la URL de la imagen
                     else:
-                        data['imagen'] = None  # Si no se encuentra la imagen, asigna None
+                        data["imagen"] = (
+                            None  # Si no se encuentra la imagen, asigna None
+                        )
 
                     # Extraer todos los comentarios
                     comentarios = []
-                    comentario_tags = product_soup.find_all('p', {'class': 'ui-review-capability-comments__comment__content'})
-                    
+                    comentario_tags = product_soup.find_all(
+                        "p",
+                        {"class": "ui-review-capability-comments__comment__content"},
+                    )
+
                     if comentario_tags:
                         for comentario in comentario_tags:
                             comentarios.append(comentario.text.strip())
 
                     # Almacenar comentarios si existen, de lo contrario asignar None
-                    data['comentarios'] = comentarios if comentarios else None
+                    data["comentarios"] = comentarios if comentarios else None
 
                 else:
-                    data['imagen'] = None
-                    data['comentarios'] = None
+                    data["imagen"] = None
+                    data["comentarios"] = None
             except Exception as e:
                 print(f"Error al hacer la solicitud de la imagen y comentarios: {e}")
-                data['imagen'] = None
-                data['comentarios'] = None
-        
-            
-                    
+                data["imagen"] = None
+                data["comentarios"] = None
+
             products_array.append(data)
-            #print(data)
-            
+            # print(data)
 
     # Convertir a DataFrame y mostrar
     df = pd.DataFrame(products_array)
     print(df)
 
-    archivo_json = 'productos.json'
-    with open(archivo_json, 'w') as file:
+    archivo_json = "productos.json"
+    with open(archivo_json, "w", encoding="utf-8") as file:
         json.dump(products_array, file, indent=4, ensure_ascii=False)
--- app.py
+++ app.py
@@ -1,54 +1,139 @@
 import os
 from flask import Flask, render_template, flash, redirect, url_for
-from IAProcess.recognizeProduct import ProcessInformation 
+from IAProcess.recognizeProduct import ProcessInformation
 from IAProcess.rankProcess.rankProduct import rankProduct
 from IAProcess.Web_Scrape.indexscrapping import scrapping
 from config.config import Config
+from flask import flash
 from utils.db import db  # Importa desde extensiones
 from templates.formsApp.form import FormSearchProduct
 from models.producto import Producto
 from models.opinion import Opinion  # Asegúrate de importar Opinion si lo usas
 
-app = Flask(__name__, static_folder='static')
-app.config['SECRET_KEY'] = 'KKJRE54573FSLKD*DF5FDLOLYSVVFW83472386JXT'
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
+app = Flask(__name__, static_folder="static")
+app.config["SECRET_KEY"] = "KKJRE54573FSLKD*DF5FDLOLYSVVFW83472386JXT"
+app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
 
 # Inicializa la base de datos
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+class NoProductsFoundException(Exception):
+    """Excepción personalizada para cuando no se encuentran productos"""
+
+    pass
+
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     form = FormSearchProduct()
-    print('nombre del producto:')
-    data = input()
-    scrapping(data)
-    if form.validate_on_submit():
-        try:
-            flash(f'Form submitted successfully! Name: {form.productName.data}', 'success')
-            # product = ProcessInformation(form.productName.data)
-            # nombre_producto = product['nombre']
-            
-            # scrapping(nombre_producto)
-            return redirect(url_for('products'))  # Redirige a una página de resultados
-        except Exception as e:
-            flash(f'Ocurrió un error: {str(e)}', 'danger')
-    return render_template('search.html', form=form)
 
-@app.route('/products')
+    if form.validate_on_submit():
+        product_name = form.productName.data
+        print(
+            "nombre del producto:", product_name
+        )  # Asegúrate de que se imprime el nombre correcto
+
+        try:
+            # Llama a la función de scraping y espera los resultados
+            result = scrapping(product_name)  # Cambia `data` por `product_name`
+
+            # Verifica si la lista de productos no está vacía
+            if not result or len(result) == 0:
+                raise NoProductsFoundException("No se encontraron productos")
+
+            for product in result:
+                if product:
+                    # Verifica si el producto ya existe en la base de datos por nombre o URL
+                    existing_product = Producto.query.filter(
+                        (Producto.nombre == product["nombre_articulo"])
+                        | (Producto.url_producto == product["link"])
+                    ).first()
+
+                    if existing_product:
+                        print(
+                            f"El producto '{existing_product.nombre}' ya existe en la base de datos.",
+                            "info",
+                        )
+                        continue  # No guarda el producto si ya existe, sigue con el siguiente
+
+                    # Si no existe, lo guarda en la base de datos
+                    precio = (
+                        float(product["precio_antes"].replace("$", "").replace(",", ""))
+                        if product.get("precio_antes")
+                        else 0.0
+                    )
+                    valoracion = (
+                        float(product["calificacion"])
+                        if "calificacion" in product
+                        else None
+                    )
+
+                    new_product = Producto(
+                        nombre=product["nombre_articulo"],
+                        precio=precio,
+                        image_url=product.get("imagen"),
+                        url_producto=product["link"],
+                        valoracion=valoracion,
+                    )
+                    db.session.add(new_product)
+                    db.session.commit()  # Guarda el producto primero
+
+                    # Guardar los comentarios asociados (si existen)
+                    for comentario in product.get("comentarios", []):
+                        new_opinion = Opinion(
+                            contenido=comentario, producto_id=new_product.id
+                        )
+                        db.session.add(new_opinion)
+
+                    db.session.commit()  # Guarda todas las opiniones
+
+        except NoProductsFoundException as e:
+            print(str(e))
+            flash("No se encontraron productos con ese nombre.", "danger")
+            return render_template("search.html", form=form)
+
+        except Exception as e:
+            print(f"Error durante el procesamiento: {str(e)}")
+            flash("Ocurrió un error inesperado.", "danger")
+            return render_template("search.html", form=form)
+
+    return render_template("search.html", form=form)
+
+    # if form.validate_on_submit():
+    #     try:
+    #         flash(f'Form submitted successfully! Name: {form.productName.data}', 'success')
+    #         # product = ProcessInformation(form.productName.data)
+    #         # nombre_producto = product['nombre']
+
+    #         # scrapping(nombre_producto)
+    #         return redirect(url_for('products'))  # Redirige a una página de resultados
+    #     except Exception as e:
+    #         flash(f'Ocurrió un error: {str(e)}', 'danger')
+    # return render_template('search.html', form=form)
+
+
+@app.route("/products")
 def products():
     try:
-        products = Producto.query.all()  # Obtiene todos los productos de la base de datos
-        ranked_products = rankProduct([product.to_dict() for product in products])  # Asegúrate de tener un método to_dict en tu modelo
-        return render_template('products.html', productos=ranked_products)
+        products = (
+            Producto.query.all()
+        )  # Obtiene todos los productos de la base de datos
+        ranked_products = rankProduct(
+            [product.to_dict() for product in products]
+        )  # Asegúrate de tener un método to_dict en tu modelo
+        return render_template("products.html", productos=ranked_products)
     except Exception as e:
-        flash(f'Ocurrió un error al obtener los productos: {str(e)}', 'danger')
-        return redirect(url_for('index'))
+        flash(f"Ocurrió un error al obtener los productos: {str(e)}", "danger")
+        return redirect(url_for("index"))
 
-@app.route('/products/details/<int:id>')
+
+@app.route("/products/details/<int:id>")
 def itemDetails(id):
-    return render_template('productDetails.html', product_id=id)
+    return render_template("productDetails.html", product_id=id)
